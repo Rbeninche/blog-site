1afbad85b83597183de84be50c150b9c766257b7
Rbeninche <rbeninche@gmail.com>
2021-08-11 22:23:25 -0400

    updated some packages

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.jinja_env.filters['zip'] = zip
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blog:password123@localhost:8889/blog'
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
