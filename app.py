1ad825e10dffb7b03b01ba9f9920c02aa6218e7f
Rbeninche <rbeninche@gmail.com>
2021-09-21 21:45:00 -0400

    added procfile

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from dbconnect import connection_string
 
 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.jinja_env.filters['zip'] = zip
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blog:password123@localhost:8889/blog'
+
+ENV = 'prod'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blog:password123@localhost:8889/blog'
+
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
+
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
